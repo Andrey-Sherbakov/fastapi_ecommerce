4009f15d46a55fae49ad66e1e7cf54568f10de88
Andrey-Sherbakov <63076931+Andrey-Sherbakov@users.noreply.github.com>
2025-05-26 10:26:57 +0300

    create superuser endpoint

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -6,44 +6,69 @@ from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import insert
 
 from app.backend.db_depends import DbSessionDep
-from app.helpers.auth import get_current_user_payload, authenticate_user, create_access_token, bcrypt_context
+from app.helpers.auth import (
+    get_current_user_payload,
+    authenticate_user,
+    create_access_token,
+    bcrypt_context,
+)
 from app.models import User
 from app.schemas import CreateUser
 
-router = APIRouter(prefix='/auth', tags=['auth'])
+router = APIRouter(prefix="/auth", tags=["auth"])
 
 
-@router.post('/token')
+@router.post("/token")
 async def login(db: DbSessionDep, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     user = await authenticate_user(db, form_data.username, form_data.password)
 
-    token = await create_access_token(user.username, user.id, user.is_admin, user.is_supplier, user.is_customer,
-                                      expires_delta=timedelta(minutes=20))
+    token = await create_access_token(
+        user.username,
+        user.id,
+        user.is_admin,
+        user.is_supplier,
+        user.is_customer,
+        expires_delta=timedelta(minutes=20),
+    )
 
-    return {
-        'access_token': token,
-        'token_type': 'bearer'
-    }
+    return {"access_token": token, "token_type": "bearer"}
 
 
-@router.post('/', status_code=status.HTTP_201_CREATED)
+@router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: DbSessionDep, new_user: CreateUser):
-    await db.execute(insert(User).values(
-        first_name=new_user.first_name,
-        last_name=new_user.last_name,
-        username=new_user.username,
-        email=new_user.email,
-        hashed_password=bcrypt_context.hash(new_user.password),
-    ))
+    await db.execute(
+        insert(User).values(
+            first_name=new_user.first_name,
+            last_name=new_user.last_name,
+            username=new_user.username,
+            email=new_user.email,
+            hashed_password=bcrypt_context.hash(new_user.password),
+        )
+    )
 
     await db.commit()
 
-    return {
-        'status_code': status.HTTP_201_CREATED,
-        'transaction': 'Successful'
-    }
+    return {"status_code": status.HTTP_201_CREATED, "transaction": "Successful"}
 
 
-@router.get('/read_current_user')
+@router.post("/superuser", status_code=status.HTTP_201_CREATED)
+async def create_superuser(db: DbSessionDep, new_user: CreateUser):
+    await db.execute(
+        insert(User).values(
+            first_name=new_user.first_name,
+            last_name=new_user.last_name,
+            username=new_user.username,
+            email=new_user.email,
+            hashed_password=bcrypt_context.hash(new_user.password),
+            is_admin=True,
+        )
+    )
+
+    await db.commit()
+
+    return {"status_code": status.HTTP_201_CREATED, "transaction": "Successful"}
+
+
+@router.get("/read_current_user")
 async def read_current_user(user: str = Depends(get_current_user_payload)):
-    return {'User': user}
+    return {"User": user}
